7970dd831b71ae0829cc56b0f3f3150b43b28c1a
Izznakim <IzzuddinNaufalHakim@gmail.com>
2021-11-18 08:40:05 +0700

    Perbaikan pada menu untuk logout

--- app/src/main/java/com/firmansyah/malangcamp/admin/AdminHomeActivity.kt
+++ app/src/main/java/com/firmansyah/malangcamp/admin/AdminHomeActivity.kt
@@ -1,24 +1,34 @@
 package com.firmansyah.malangcamp.admin
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.firmansyah.malangcamp.HomeActivity
 import com.firmansyah.malangcamp.R
 import com.firmansyah.malangcamp.databinding.ActivityAdminHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class AdminHomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAdminHomeBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityAdminHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth= Firebase.auth
 
         val navView: BottomNavigationView = binding.navView
 
@@ -32,5 +42,23 @@ class AdminHomeActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.profil_admin,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.actionKeluar -> {
+                auth.signOut()
+                Intent(this, HomeActivity::class.java).also { intent ->
+                    intent.flags= Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
