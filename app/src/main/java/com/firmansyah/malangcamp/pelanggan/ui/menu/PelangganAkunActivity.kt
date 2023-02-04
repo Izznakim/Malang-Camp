c857b0a11019721a9fccca33a7a90c97f467e603
Izznakim <IzzuddinNaufalHakim@gmail.com>
2023-02-05 06:58:51 +0700

    - PeraturanSewaActivity.kt has migration to Compose.

--- app/src/main/java/com/firmansyah/malangcamp/pelanggan/ui/menu/PelangganAkunActivity.kt
+++ app/src/main/java/com/firmansyah/malangcamp/pelanggan/ui/menu/PelangganAkunActivity.kt
@@ -18,6 +18,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -74,7 +76,13 @@ class PelangganAkunActivity : AppCompatActivity() {
 
             MalangCampTheme {
                 Surface {
-                    Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+                    Column(
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp)
+                            .verticalScroll(
+                                rememberScrollState()
+                            )
+                    ) {
                         if (viewModel.isLoading.value) {
                             LinearProgressBar(alpha = 1f)
                         } else {
